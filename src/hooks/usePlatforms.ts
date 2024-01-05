23fd75c8d99fa1e4635f940fef0e1f96b74d6792
izzatonline <izzat.online@gmail.com>
2024-01-05 15:10:42 +0800

    Refactoring: Fetch games using React Query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import useData, { FetchResponse } from "./useData";
-import apiClient from "../services/api-client";
 import platforms from "../data/platforms";
+import apiClient, { FetchResponse } from "../services/api-client";
 
 interface Platform {
   id: number;
